3671559a53dc7cfed2fb580f9e16de9e90c2bb76
Oleg-root <oleg_filipyev@mail.ru>
2022-05-20 18:28:32 +0300

    Setting up...

--- django_project/urls.py
+++ django_project/urls.py
@@ -28,8 +28,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('', include('test_app.urls')),
-    path('^inbox/notifications/', include(notifications.urls, namespace='notifications'))
+    path('', include('test_app.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
